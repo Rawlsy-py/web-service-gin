85f4997a765c6f102ec2cf947e384fe08e4d9634
Rawlsy-py <christopher_m_mitchell@live.co.uk>
2023-10-14 16:59:59 +0100

    change scope

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"web-service-gin/database"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./example.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = database.CreateTables(db)
+	if err != nil {
+		panic(err)
+	}
+
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+	router.Run(":8080")
 }
